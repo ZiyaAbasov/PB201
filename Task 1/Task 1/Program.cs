7316ff1dfcbf8db9a5d461f6833a882768dce462
ZiyaAbasov <ziyaabasov28@gmail.com>
2024-05-11 19:03:43 +0400

    added Task1 file

---
+++ Task 1/Task 1/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region
+            for (int i = 1; i <= 100; i++)
+            { 
+                if (i % 5 == 0 && i % 3 == 0) 
+                {
+                    Console.WriteLine($"{i} is divisible by both 3 and 5");
+                }
+                else
+                {
+                    Console.WriteLine($"{i} is not divisible by both 3 and 5");
+                }
+                #endregion
+
+            }
+        }
+    }
+}
---
+++ Task 2/Task 2/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region
+
+            var num = 84;
+
+
+            if(85 > num && num > 65)
+            {
+                Console.WriteLine("You will get Certificate");
+            }
+            else if(95 > num && num > 85)
+            { 
+                Console.WriteLine("You will get Honour Certificate");
+            }
+            else if (num > 95)
+            {
+                Console.WriteLine("Congratulations you will get High Honour Certificate");
+            }
+            else
+            {
+                Console.WriteLine("You Fail"); 
+            }
+
+
+            int num1 = 86;
+
+            if(num1 > 95)
+            {
+                Console.WriteLine("Congratulations you will get High Honour Certificate");
+            }
+            else if(95 > num1 && num1 > 85) 
+            {
+                Console.WriteLine("You will get Honour Certificate");
+            }
+            else if(85 > num1 && num1 > 65)
+            {
+                Console.WriteLine("You will get Certificate");
+            }
+            else
+            {
+                Console.WriteLine("You Fail");
+            }
+             #endregion
+        }
+          
+    }
+}
